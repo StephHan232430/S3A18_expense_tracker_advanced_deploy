dcb61e111f822153b317bae005e0699112b01e98
Steph <blackman914037@gmail.com>
2019-12-09 21:17:46 +0800

    Adjust seeder

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -15,12 +15,16 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 
-  for (let uNum = 0; uNum < userSeeds.length; uNum++) {
+  userSeeds.forEach(user => {
     bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(userSeeds[uNum].password, salt, (err, hash) => {
-        if (err) throw err
-        userSeeds[uNum].password = hash
-        User.create(userSeeds[uNum]).then(user => {
+      bcrypt.hash(userSeeds.password, salt, (err, hash) => {
+        const newUser = new User({
+          name: userSeeds.name,
+          email: userSeeds.email,
+          password: hash
+        })
+
+        newUser.save().then(user => {
           for (let rNum = uNum * 5; rNum < (uNum + 1) * 5; rNum++) {
             Record.create({
               name: recordSeeds[rNum].name,
@@ -31,13 +35,30 @@ db.once('open', () => {
               userId: user._id
             })
           }
-          console.log('seeded!')
         }).catch(err => console.log(err))
       })
     })
-  }
+  })
+
+  // for (let uNum = 0; uNum < userSeeds.length; uNum++) {
+  //   bcrypt.genSalt(10, (err, salt) => {
+  //     bcrypt.hash(userSeeds[uNum].password, salt, (err, hash) => {
+  //       if (err) throw err
+  //       userSeeds[uNum].password = hash
+  //       User.create(userSeeds[uNum]).then(user => {
+  //         for (let rNum = uNum * 5; rNum < (uNum + 1) * 5; rNum++) {
+  //           Record.create({
+  //             name: recordSeeds[rNum].name,
+  //             merchant: recordSeeds[rNum].merchant,
+  //             category: recordSeeds[rNum].category,
+  //             amount: recordSeeds[rNum].amount,
+  //             date: recordSeeds[rNum].date,
+  //             userId: user._id
+  //           })
+  //         }
+  //       }).catch(err => console.log(err))
+  //     })
+  //   })
+  // }
+  console.log('seeded!')
 })
-
-  // || 'mongodb://localhost/expense-tracker-advanced'
-
-// 'mongodb://heroku_mx3xc41d:6ebk34pf5hjumbg5lqvcq1c2p3@ds013212.mlab.com:13212/heroku_mx3xc41d'
